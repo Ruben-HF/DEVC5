8ac6f01e09c05e3a52fec9da97be60a631e6d306
Rubén Emmanuel Herrera Flores <111837256+Ruben-HF@users.noreply.github.com>
2023-02-13 08:12:03 -0600

    Update Tools 13-02
    Actualización en Tools

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 console.log('Hola')
 
+//TODO LIST
+
 function guardar(){
     let texto = document.getElementById('textotext').value
     
@@ -92,3 +94,77 @@ function salir(e){
 
 render()
 
+
+//NOTAS
+
+// Get references to elements
+const notesList = document.querySelector('#notes-list');
+const noteTitleInput = document.querySelector('#note-title-input');
+const noteTextInput = document.querySelector('#note-text-input');
+const saveBtn = document.querySelector('#save-btn');
+
+// Load any existing notes from local storage
+const notes = JSON.parse(localStorage.getItem('notes')) || [];
+
+// Render the notes list
+function renderNotesList() {
+  notesList.innerHTML = '';
+  for (let i = 0; i < notes.length; i++) {
+    const note = notes[i];
+    const noteEl = document.createElement('div');
+    noteEl.classList.add('note');
+    noteEl.innerHTML = `
+      <div class="note-title">${note.title}</div>
+      <div class="note-actions">
+        <button class="edit-btn btn btn-outline-success" style="width: 45%;">Visualizar</button>
+        <button class="delete-btn btn btn-outline-danger" style="width: 45%;">Eliminar</button>
+      </div>
+    `;
+    notesList.appendChild(noteEl);
+  }
+}
+
+// Save the notes to local storage
+function saveNotes() {
+  localStorage.setItem('notes', JSON.stringify(notes));
+}
+
+// Handle the save button click
+saveBtn.addEventListener('click', function() {
+  notes.push({
+    title: noteTitleInput.value,
+    text: noteTextInput.value
+  });
+  noteTitleInput.value = '';
+  noteTextInput.value = '';
+  renderNotesList();
+  saveNotes();
+});
+
+// Handle the edit button click
+notesList.addEventListener('click', function(event) {
+  if (event.target.classList.contains('edit-btn')) {
+    const noteEl = event.target.parentElement.parentElement;
+    const noteIndex = Array.from(notesList.children).indexOf(noteEl);
+    const note = notes[noteIndex];
+    noteTitleInput.value = note.title;
+    noteTextInput.value = note.text;
+    notes.splice(noteIndex, 1);
+    renderNotesList();
+    saveNotes();
+  }
+});
+
+// Handle the delete button click
+notesList.addEventListener('click', function(event) {
+  if (event.target.classList.contains('delete-btn')) {
+    const noteEl = event.target.parentElement.parentElement;
+    const noteIndex = Array.from(notesList.children).indexOf(noteEl);
+    notes.splice(noteIndex, 1);
+    renderNotesList();
+    saveNotes();
+  }
+});
+
+// Render the notes list for the first time
+renderNotesList();
